81814236f5a91aebec9b81c12bc479405ff90ce4
Max Kinet <maxkinet@gmail.com>
2026-01-18 14:12:55 -0800

    add spaceships

--- src/scenes/Play.js
+++ src/scenes/Play.js
@@ -14,9 +14,13 @@ class Play extends Phaser.Scene {
         this.add.rectangle(0, game.config.height - borderUISize, game.config.width, borderUISize, 0xFFFFFF).setOrigin(0, 0);
         this.add.rectangle(0, 0, borderUISize, game.config.height, 0xFFFFFF).setOrigin(0, 0);
         this.add.rectangle(game.config.width - borderUISize, 0, borderUISize, game.config.height, 0xFFFFFF).setOrigin(0, 0);
-        
+
         // Add p1 rocket
         this.p1Rocket = new Rocket(this, game.config.width/2, game.config.height - borderUISize - borderPadding, 'rocket').setOrigin(0.5, 0);
+        // Add 3 spaceships
+        this.ship01 = new Spaceship(this, game.config.width + borderUISize*6, borderUISize*4, 'spaceship', 0, 30).setOrigin(0, 0);
+        this.ship02 = new Spaceship(this, game.config.width + borderUISize*3, borderUISize*5 + borderPadding*2, 'spaceship', 0, 20).setOrigin(0,0);
+        this.ship03 = new Spaceship(this, game.config.width, borderUISize*6 + borderPadding*4, 'spaceship', 0, 10).setOrigin(0,0);
 
         // Define keys
         keyFIRE = this.input.keyboard.addKey(Phaser.Input.Keyboard.KeyCodes.F);
@@ -29,5 +33,9 @@ class Play extends Phaser.Scene {
         this.starfield.tilePositionX -= 4;
         // Run p1Rocket's update loop
         this.p1Rocket.update();
+        // Run spaceships' update loops
+        this.ship01.update();
+        this.ship02.update();
+        this.ship03.update();
     }
 }
