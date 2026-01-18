9502409562aaf73fb88797dcc655aa0681bfdb8f
Max Kinet <maxkinet@gmail.com>
2026-01-17 23:22:10 -0800

    add assets

--- src/scenes/Menu.js
+++ src/scenes/Menu.js
@@ -4,6 +4,16 @@ class Menu extends Phaser.Scene {
         super("menuScene");
     }
 
+    preload() {
+        // Load images and tile sprites
+        // We can load assets that we plan on using in future scenes
+        // because Phaser provides a persistent cache, which holds all loaded
+        // assets
+        this.load.image("rocket", "../../assets/rocket.png");
+        this.load.image("spaceship", "../../assets/spaceship.png");
+        this.load.image("starfield", "../../assets/starfield.png");
+    }
+
     create() {
         this.add.text(20, 20, "Rocket Patrol Menu");
         this.scene.start("playScene");
