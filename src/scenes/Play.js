6dece62ef92f0d7ee4cea2d79bbcfa6caa09ea9a
Max Kinet <maxkinet@gmail.com>
2026-01-18 15:30:41 -0800

    add score

--- src/scenes/Play.js
+++ src/scenes/Play.js
@@ -27,6 +27,21 @@ class Play extends Phaser.Scene {
         keyRESET = this.input.keyboard.addKey(Phaser.Input.Keyboard.KeyCodes.R);
         keyLEFT = this.input.keyboard.addKey(Phaser.Input.Keyboard.KeyCodes.LEFT);
         keyRIGHT = this.input.keyboard.addKey(Phaser.Input.Keyboard.KeyCodes.RIGHT);
+        // Init score
+        this.p1Score = 0;
+        let scoreConfig = {
+            fontFamily: 'Courier',
+            fontSize: '28px',
+            backgroundColor: '#F3B141',
+            color: '#843605',
+            align: 'right',
+            padding: {
+                top: 5,
+                bottom: 5,
+            },
+            fixedWidth: 100
+        }
+        this.scoreLeft = this.add.text(borderUISize + borderPadding, borderUISize + borderPadding * 2, this.p1Score, scoreConfig);
     }
 
     update() {
@@ -80,5 +95,8 @@ class Play extends Phaser.Scene {
             ship.alpha = 1;
             boom.destroy();
         })
+        // Add to score and update text
+        this.p1Score += ship.points;
+        this.scoreLeft.text = this.p1Score;
     }
 }
